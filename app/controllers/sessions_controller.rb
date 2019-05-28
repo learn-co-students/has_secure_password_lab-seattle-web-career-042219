e63e5bea301a1dd64b0181729351d94baee4e12b
github username <gregm9999@gmail.com>
2019-05-27 19:47:15 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,22 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-    def new
-    end
+  def new; end
 
-    def create
-        @user = User.find_by(name: params[:user][:name])
-        return head(:forbidden) unless @user.authenticate(params[:user][:password])
-        if @user
-            session[:user_id] = @user.id
-            redirect_to(controller: 'welcome', action: 'home')
-        else
-            redirect_to(controller: 'sessions', action: 'new')
-        end
-      end
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
 
-    def destroy
-        session.delete :name
-        redirect_to '/'
+    if @user
+      session[:user_id] = @user.id
+      redirect_to(controller: 'welcome', action: 'home')
+    else
+      redirect_to(controller: 'sessions', action: 'new')
     end
+end
+
+  def destroy
+    session.delete :name
+    redirect_to '/'
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  def new; end
 
-    def new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to(controller: 'welcome', action: 'home')
+    else
+      redirect_to(controller: 'users', action: 'new')
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to(controller:'welcome', action:'home')
-        else
-            redirect_to(controller: 'users', action: 'new')
-        end
-    end
-     
-    private
-     
-      def user_params
-        params.require(:user).permit(:name, :password, :password_confirmation)
-      end
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
